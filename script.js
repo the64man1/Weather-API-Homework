a49a409d07e79b24debbeab737e4566950b39b68
maxquintanilla <the64man1@yahoo.com>
2021-04-10 14:49:12 -0500

    modified urls

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ window.addEventListener('load', function () {
               var imgEl = document.createElement('img');
               imgEl.setAttribute(
                 'src',
-                `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
+                `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`
               );
               var p1El = document.createElement('p');
               p1El.classList.add('card-text');
@@ -76,7 +76,7 @@ window.addEventListener('load', function () {
   
     function getUVIndex(lat, lon) {
       fetch(
-        `http://api.openweathermap.org/data/2.5/uvi?appid=d91f911bcf2c0f925fb6535547a5ddc9&lat=${lat}&lon=${lon}`
+        `https://api.openweathermap.org/data/2.5/uvi?appid=d91f911bcf2c0f925fb6535547a5ddc9&lat=${lat}&lon=${lon}`
       )
         .then((res) => res.json())
         .then((data) => {
